ddb626a845bd3dbbc579f54646831650de25b94b
Fabs713 <f.marasco123@gmail.com>
2024-06-19 21:25:01 +0200

    Created a system to keep score and the core code for the game to work.

--- script.js
+++ script.js
@@ -1,18 +1,17 @@
 // Randomize NPC choice //
 function getComputerChoice() {
-         cpuChoice = Math.floor(Math.random() * 3);
-        if (cpuChoice === 0) {
-            rps = "Rock";
-        } else if (cpuChoice === 1) {
-            rps = "Paper";
-        } else {
-            rps = "Scissors";
-        }
-        return rps;
+     cpuChoice = Math.floor(Math.random() * 3);
+    if (cpuChoice === 0) {
+            cpuRPS = "Rock";
+    } else if (cpuChoice === 1) {
+            cpuRPS = "Paper";
+    } else {
+            cpuRPS = "Scissors";
+    }
+    return cpuRPS;
 }
 getComputerChoice();
-console.log(getComputerChoice());
-
+console.log(cpuRPS);
 // Prompt for Human's choice //
 function getHumanChoice() {
     huChoice = prompt("0 for Rock, 1 for Paper, 2 for Scissors");
@@ -28,6 +27,66 @@ function getHumanChoice() {
     }
     return huRPS;
 
+} 
+getHumanChoice();
+let humanScore = 0;
+let cpuScore = 0; 
+
+
+function playRound(huRPS, cpuRPS) {
+    let win = 0;
+    let lose = 0;
+    if (cpuRPS == "Rock") {
+
+        if (huRPS == "Rock") {
+            console.log("Tie round!");   
+        } else if (huRPS == "Paper") {
+            console.log("You win! Paper beats Rock!");
+            win = 1;
+        } else if (huRPS == "Scissors") {
+            console.log("You lose! Rock beats Scissors!");
+            lose = 1;
+        }
+
+    } else if (cpuRPS == "Paper") {
+        if (huRPS == "Rock") {
+            console.log("You lose! Paper beats Rock!");
+            lose = 1; 
+        } else if (huRPS == "Paper") {
+            console.log("Tie round!");
+        } else if (huRPS == "Scissors") {
+            console.log("You win! Scissors beats Paper!");
+            win = 1;
+        }
+
+    } else if (cpuRPS == "Scissors") {
+        if (huRPS == "Rock") {
+            console.log("You win! Rock beats Scissors!");
+            win = 1;
+            return win;   
+        } else if (huRPS == "Paper") {
+            console.log("You lose! Scissors beats Paper!");
+            lose = 1;
+        } else if (huRPS == "Scissors") {
+            console.log("Tie round!");
+        }
+    }
+    
+    if (win === 1) {
+        humanScore++;
+    } else if (lose === 1 && humanScore > 0) {
+        humanScore--;
+        cpuScore++;
+    } else if (lose === 1) {
+        cpuScore++;
+    }
+    
 }
 
-console.log(getHumanChoice());
+    
+
+    
+
+playRound(huRPS, cpuRPS);
+console.log("Your score: ", humanScore);
+console.log("CPU: ", cpuScore);
